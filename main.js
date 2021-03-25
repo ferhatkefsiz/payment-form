a9d312c4d92da11b334dc7ab132480227fc64f68
ferhat kefsiz <ferhatkefsiz7@yahoo.com>
2021-03-26 01:10:56 +0300

    Payment section and form is created

--- main.js
+++ main.js
@@ -3,5 +3,23 @@
 // Pop up function for product
 
 function popup() {
-  window.open('/ödeme.html', "Ödeme", 'width=1920,height=800,left=250,top=100,scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no')
+  window.open('/ödeme.html', 'width=1920,height=800,left=250,top=100,scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no')
+}
+
+
+// Ödeme.html 
+
+/* Optional Javascript to close the radio button version by clicking it again */
+var myRadios = document.getElementsByName('tabs2');
+var setCheck;
+var x = 0;
+for (x = 0; x < myRadios.length; x++) {
+  myRadios[x].onclick = function () {
+    if (setCheck != this) {
+      setCheck = this;
+    } else {
+      this.checked = false;
+      setCheck = null;
+    }
+  };
 }
