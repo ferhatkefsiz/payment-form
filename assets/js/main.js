3ad07478415b3623718354c58ccdd664d884cec4
ferhat kefsiz <ferhatkefsiz7@yahoo.com>
2021-03-27 20:14:36 +0300

    instant transfer section is completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 // Pop up function for product
 
 function popup() {
-  window.open('/html/ödeme.html',"Payment Form", 'width=1856,height=800,left=250,top=100,scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no')
+  window.open('/html/paymentform.html', 'width=1856,height=800,left=250,top=100,scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no')
 }
 
 
